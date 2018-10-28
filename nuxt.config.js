abcf68c84a15b33749255746915716d6bce076de
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-10-29 00:07:53 +0900

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,35 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Hello WAKABA.WORLD :)'
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'Wakaba Yamaguchi, 山口若葉, ポートフォリオ, Portfolio'
+      },
+      { hid: 'og:title', name: 'og:title', content: 'WAKABA.WORLD' },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:url', name: 'og:url', content: 'https://wakaba.world' },
+      { hid: 'og:locale', name: 'og:locale', content: 'ja_JP' },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'Hello WAKABA.WORLD :)'
+      },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'WAKABA.WORLD' },
+      {
+        rel: 'stylesheet',
+        href:
+          '///maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Rubik'
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
@@ -24,7 +52,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [],
+  css: ['~/assets/css/lib.css', '~/assets/css/app.css'],
 
   /*
   ** Plugins to load before mounting the App
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,162 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        Welcome to WAKABA.WORLD :)
+  <section class="Container">
+    <div class="Section">
+      <h1>
+        WAKABA YAMAGUCHI
       </h1>
+
+      <img
+        class="ProfileIcon"
+        src="~/assets/img/profile-icon.jpg"
+        alt="wakaba yamaguchi profile icon">
+    </div>
+
+    <div class="Section">
+      <h2>Communicator / Curator / Writer / Editor</h2>
+
+      <div class="Section__Text">
+        愛知県出身。人と人とがつながり自由になるビル、NagatachoGRiDのコミュニケーター兼、フリーのエディター。
+        「カッコいい生き様を全力で応援する」をモットーに、ブランドメイクやイベント企画、コミュニティサポートを行っている。
+        学生時代に国内にて学芸員資格を取得。その後、アメリカにて美術館とギャラリーでインターン、現地の大学にて学芸員学課程修了後、帰国。ITベンチャーにてテクニカルアドバイザーとして従事後、現在の活動をはじめる。
+      </div>
+    </div>
+
+    <div class="Section">
+      <h2>Contacts</h2>
+      <ul class="List">
+        <li
+          v-for="(s, index) in snsLinks"
+          :key="index"
+          class="ListItem">
+          <a :href="s.url">{{ s.text }}</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="Section">
+      <h2>JOB / PROJECTS</h2>
+      <table class="Table">
+        <tr
+          v-for="(p, index) in projects"
+          :key="index"
+          class="Table__Row">
+          <td class="Table__Column">
+            {{ p.date }}
+          </td>
+          <td class="Table__Column">
+            <a
+              :href="p.url"
+              target="_blank">{{ p.name }}</a>
+          </td>
+          <td class="Table__Column">
+            {{ p.role }}
+          </td>
+        </tr>
+      </table>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  computed: {
+    snsLinks: () => [
+      { url: 'https://www.facebook.com/wakaba.yamaguchi.1', text: 'Facebook' },
+      { url: 'https://www.facebook.com/wakaba.yamaguchi.1', text: 'Twitter' },
+      { url: 'https://note.mu/wakabaworld', text: 'Note' }
+    ],
+    projects: () => [
+      {
+        date: '2018/10',
+        name: '千代田区100人カイギ',
+        role: 'イベント企画・運営',
+        url: 'https://chiyoda100.peatix.com/'
+      },
+      {
+        date: '2018/09',
+        name: 'JA農協　青森米活　キャンペーン',
+        role: '執筆（ライター）',
+        url: 'https://aomori-komekatu.com/'
+      },
+      {
+        date: '2018/08',
+        name: 'KAI-YOU',
+        role: 'カルチャーメディア執筆（ライター・編集）',
+        url: 'http://kai-you.net/'
+      },
+      {
+        date: '2018/07',
+        name: '代官山ロータリークラブ',
+        role: 'サイト、SNS管理・イベント補佐(事務局担当者）',
+        url: 'http://rctd.club/'
+      },
+      {
+        date: '2018/04',
+        name: 'GRID CINEMA NITE',
+        role: '屋上映画企画・広報・運営',
+        url: 'https://www.facebook.com/gridcinemanite'
+      },
+      {
+        date: '2018/04',
+        name: '未知の街nagatacho',
+        role: '企画立ち上げ・サイト運営',
+        url: 'https://www.facebook.com/未知の街nagatacho'
+      },
+      {
+        date: '2018/02',
+        name: 'GRID 1st aniversery',
+        role: 'イベント企画・運営',
+        url: 'https://grid.tokyo.jp/note/406'
+      },
+      {
+        date: '2017/10',
+        name: '生き方見本市',
+        role: '登壇',
+        url: 'https://www.ikikata-mihonichi.com/top/'
+      },
+      {
+        date: '2017/09',
+        name: '株式会社レクター',
+        role: 'コンサルティング補佐',
+        url: 'http://rector.co.jp/company/'
+      },
+      {
+        date: '2017/04',
+        name: '国立国会図書館',
+        role: '調査業務',
+        url: 'http://www.ndl.go.jp/'
+      },
+      {
+        date: '2017/02',
+        name: 'GRiD opening party',
+        role: 'イベント統括',
+        url: 'https://www.facebook.com/events/303645783366856/'
+      },
+      {
+        date: '2017/01',
+        name: 'Nagatacho GRiD',
+        role: '広報・オウンドメディア運営',
+        url: 'https://grid.tokyo.jp/'
+      },
+      {
+        date: '2016/08',
+        name: '一般社団法人Japan Elixir Association',
+        role: '会計・イベント統括（代表監事）',
+        url: 'https://elixir-fest.jp/'
+      }
+    ]
   }
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+.ProfileIcon {
+  width: 240px;
+  border-radius: 50%;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media only screen and (max-width: 700px) {
+  .ProfileIcon {
+    width: 120px;
+  }
 }
 </style>
