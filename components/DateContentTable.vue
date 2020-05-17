dbf80159812dc3f1cdc62497e0ca95cd719b47af
tanakaworld <yutaro.tanaka.world@gmail.com>
2020-05-17 16:34:01 +0900

    feat: update jobs and projects

---
+++ components/DateContentTable.vue
@@ -0,0 +1,78 @@
+<template>
+  <table class="Table">
+    <tr
+      v-for="(p, index) in contents"
+      :key="index"
+      class="Table__Row">
+      <td class="Table__Column Table__ColumnDate">
+        {{ p.date }}
+      </td>
+      <td class="Table__Column Table__ColumnName">
+        <a
+          :href="p.url"
+          target="_blank">{{ p.name }}</a>
+      </td>
+      <td class="Table__Column Table__ColumnRole">
+        {{ p.role }}
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    contents: {
+      type: Array,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+.Table {
+  table-layout: auto;
+  border-collapse: separate;
+  border-spacing: 16px;
+}
+
+.Table__Column {
+  text-align: left;
+  padding-right: 24px;
+  white-space: nowrap;
+}
+
+.Table__Column:last-child {
+  padding-right: 0;
+  white-space: normal;
+}
+
+.Table__ColumnDate {
+  line-height: 1;
+  min-width: 130px;
+}
+
+.Table__ColumnName,
+.Table__ColumnRole {
+  line-height: 1.5;
+}
+
+.Table__ColumnRole {
+  min-width: 200px;
+}
+
+@media only screen and (max-width: 700px) {
+  .Table {
+    border-spacing: 8px;
+    font-size: 1.2rem;
+  }
+
+  .Table__Column {
+    white-space: normal;
+    min-width: auto;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,17 +12,17 @@
     </div>
 
     <div class="Section">
-      <h2>Communicator / Curator / Writer / Editor</h2>
+      <h2>Event Organizer / Curator / Writer / Editor</h2>
 
       <div class="Section__Text">
-        愛知県出身。人と人とがつながり自由になるビル、NagatachoGRiDのコミュニケーター兼、フリーのエディター。
+        愛知県出身。人と人とがつながり自由になるビル、Nagatacho GRiDのコミュニケーター兼、フリーのエディター。
         「カッコいい生き様を全力で応援する」をモットーに、ブランドメイクやイベント企画、コミュニティサポートを行っている。
         学生時代に国内にて学芸員資格を取得。その後、アメリカにて美術館とギャラリーでインターン、現地の大学にて学芸員学課程修了後、帰国。ITベンチャーにてテクニカルアドバイザーとして従事後、現在の活動をはじめる。
       </div>
     </div>
 
     <div class="Section">
-      <h2>Contacts</h2>
+      <h2>CONTACT</h2>
       <ul class="List">
         <li
           v-for="(s, index) in snsLinks"
@@ -34,38 +34,172 @@
     </div>
 
     <div class="Section">
-      <h2>JOB / PROJECTS</h2>
-      <table class="Table">
-        <tr
-          v-for="(p, index) in projects"
-          :key="index"
-          class="Table__Row">
-          <td class="Table__Column">
-            {{ p.date }}
-          </td>
-          <td class="Table__Column">
-            <a
-              :href="p.url"
-              target="_blank">{{ p.name }}</a>
-          </td>
-          <td class="Table__Column">
-            {{ p.role }}
-          </td>
-        </tr>
-      </table>
+      <h2>JOB</h2>
+      <DateContentTable :contents="jobs"/>
+    </div>
+
+    <div class="Section">
+      <h2>PROJECT</h2>
+      <DateContentTable :contents="projects"/>
     </div>
   </section>
 </template>
 
 <script>
+import DateContentTable from '~/components/DateContentTable.vue'
+
 export default {
+  components: {
+    DateContentTable
+  },
   computed: {
     snsLinks: () => [
       { url: 'https://www.facebook.com/wakaba.yamaguchi.1', text: 'Facebook' },
-      { url: 'https://www.facebook.com/wakaba.yamaguchi.1', text: 'Twitter' },
+      { url: 'https://twitter.com/wkwkbwb', text: 'Twitter' },
       { url: 'https://note.mu/wakabaworld', text: 'Note' }
     ],
+    jobs: () => [
+      {
+        date: '2020/03 -',
+        name: '一般社団法人日本CTO協会',
+        role: 'イベント企画・会員サポート',
+        url: 'https://cto-a.org/'
+      },
+      {
+        date: '2019/03 -',
+        name: '株式会社アドレス',
+        role: 'イベント企画・支援',
+        url: 'https://address.love/'
+      },
+      {
+        date: '2018/07 -',
+        name: '代官山ロータリークラブ',
+        role: 'サイト、SNS管理・イベント補佐(事務局担当者）',
+        url: 'http://rctd.club/'
+      },
+      {
+        date: '2017/09 - 2020/03',
+        name: '株式会社レクター',
+        role: 'コンサルティング補佐',
+        url: 'https://rector.co.jp/company/'
+      },
+      {
+        date: '2016/08 - 2020/03',
+        name: '一般社団法人Japan Elixir Association',
+        role: '会計・イベント統括（代表監事）',
+        url: 'https://rector.co.jp/company/'
+      },
+      {
+        date: '2017/03 -',
+        name: '株式会社ガイアックス',
+        role: 'NagatachoGRiD　管理人',
+        url: 'https://www.gaiax.co.jp/'
+      },
+      {
+        date: '2013/04 - 2017/01',
+        name: 'レバレジーズ株式会社',
+        role: 'エンジニア部門人材スカウトチームマネージャー',
+        url: 'https://leverages.jp/'
+      }
+    ],
     projects: () => [
+      {
+        date: '2020/05 -',
+        name: '代官山ロータリークラブ　リモート卓話',
+        role: 'イベント企画・運営',
+        url: 'http://rctd.club/'
+      },
+      {
+        date: '2020/04 -',
+        name: 'オンライン就活-学生向け合同説明会',
+        role: '講師',
+        url: 'https://onlineshukatsu.com/'
+      },
+      {
+        date: '2020/04 -',
+        name: 'オンライン就活−人事向けオンライン活用術−',
+        role: 'ファシリテーター',
+        url: 'https://www.facebook.com/events/2870656162970395/'
+      },
+      {
+        date: '2020/04',
+        name: 'STAY HOME THINK WORK-withコロナの働き方と働く場を考える-',
+        role: '登壇',
+        url: 'https://shtw-think-howtowork.peatix.com/'
+      },
+      {
+        date: '2020/03 -',
+        name: '株式会社アドレス　オンライン説明会',
+        role: 'ファシリテーション',
+        url:
+          'https://sites.google.com/address.love/event/%E8%AA%AC%E6%98%8E%E4%BC%9A%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88'
+      },
+      {
+        date: '2020/2',
+        name: 'しゅんダイアリーさん複業インタビュー',
+        role: 'インタビュイー',
+        url: 'https://youtu.be/VIHfcaskB1A'
+      },
+      {
+        date: '2020/01',
+        name: 'サイリウムダンスバトル紹介記事',
+        role: '編集',
+        url: 'https://www.cyalume-dance.com/'
+      },
+      {
+        date: '2019/11',
+        name: '100人カイギサミット',
+        role: '司会',
+        url: 'https://eventregist.com/e/100?lang=ja_JP'
+      },
+      {
+        date: '2019/06 - 2019/11',
+        name: 'GRIDROOFTOPCINEMA開催',
+        role: '主催',
+        url: 'https://motion-gallery.net/projects/grid_rooftop_cinema'
+      },
+      {
+        date: '2019/10 - 2020/06',
+        name: '代官山ロータリークラブ×ヒルサイドテラス　ラブコール企画',
+        role: 'イベント企画・運営',
+        url: 'http://hillsideterrace.com/events/8114/'
+      },
+      {
+        date: '2019/07',
+        name: 'Our Favorite Shop　猿楽祭り',
+        role: 'イベントサポート',
+        url: ''
+      },
+      {
+        date: '2019/05',
+        name: 'しゅんダイアリーさんGRiD紹介（後編）',
+        role: 'インタビュイー',
+        url: 'https://www.youtube.com/watch?v=KOdKMB2pY0M'
+      },
+      {
+        date: '2019/05',
+        name: 'しゅんダイアリーさんGRiD紹介（前編）',
+        role: 'インタビュイー',
+        url: 'https://www.youtube.com/watch?v=0gpZF8mC6kk'
+      },
+      {
+        date: '2019/03',
+        name: 'GRIDROOFTOPCINEMA クラウドファンディング',
+        role: ' 主催',
+        url: 'https://motion-gallery.net/projects/grid_rooftop_cinema'
+      },
+      {
+        date: '2019/03',
+        name: '「エンジニア採用最前線」イベント',
+        role: '主催・登壇',
+        url: 'https://rector.connpass.com/event/123315/'
+      },
+      {
+        date: '2019/03',
+        name: 'ログミー取材',
+        role: 'インタビュイー',
+        url: 'https://logmi.jp/tech/articles/320992'
+      },
       {
         date: '2018/10',
         name: '千代田区100人カイギ',
@@ -83,12 +217,6 @@ export default {
         name: 'KAI-YOU',
         role: 'カルチャーメディア執筆（ライター・編集）',
         url: 'http://kai-you.net/'
-      },
-      {
-        date: '2018/07',
-        name: '代官山ロータリークラブ',
-        role: 'サイト、SNS管理・イベント補佐(事務局担当者）',
-        url: 'http://rctd.club/'
       },
       {
         date: '2018/04',
@@ -115,12 +243,6 @@ export default {
         url: 'https://www.ikikata-mihonichi.com/top/'
       },
       {
-        date: '2017/09',
-        name: '株式会社レクター',
-        role: 'コンサルティング補佐',
-        url: 'http://rector.co.jp/company/'
-      },
-      {
         date: '2017/04',
         name: '国立国会図書館',
         role: '調査業務',
@@ -131,18 +253,6 @@ export default {
         name: 'GRiD opening party',
         role: 'イベント統括',
         url: 'https://www.facebook.com/events/303645783366856/'
-      },
-      {
-        date: '2017/01',
-        name: 'Nagatacho GRiD',
-        role: '広報・オウンドメディア運営',
-        url: 'https://grid.tokyo.jp/'
-      },
-      {
-        date: '2016/08',
-        name: '一般社団法人Japan Elixir Association',
-        role: '会計・イベント統括（代表監事）',
-        url: 'https://elixir-fest.jp/'
       }
     ]
   }
@@ -154,6 +264,15 @@ export default {
   width: 240px;
   border-radius: 50%;
 }
+
+.Table__ColumnDate {
+  width: 130px;
+}
+
+.Table__ColumnName {
+  white-space: pre-wrap;
+}
+
 @media only screen and (max-width: 700px) {
   .ProfileIcon {
     width: 120px;
